595c311170cf5bbb963b68b98fdcc721a776cf51
mendom <51889653+mendom@users.noreply.github.com>
2022-04-13 19:57:18 -0600

    Gameboard now displays

--- Connect4Project/Program.cs
+++ Connect4Project/Program.cs
@@ -8,9 +8,34 @@ namespace Connect4Project
 {
     internal class Program
     {
+        static void DisplayBoard(char [,] board)
+        {
+            //defining board parameters
+            int rows = 6;
+            int cols = 7;
+            //populating the game board
+            for(int i = 1; i <= rows; i++)
+            {
+                Console.Write("|");
+                for(int j = 1; j<=cols; j++)
+                {
+                    if(board[i,j] != 'X' && board[i,j] != 'O')
+                    {
+                        board[i, j] = '*';
+                        Console.Write(board[i, j]);
+                    }
+                    
+                }
+                Console.Write("| \n");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Marks branch");
+            char[,] board = new char[9, 9];
+            DisplayBoard(board);
+
+
+            Console.Read();
         }
     }
 }
